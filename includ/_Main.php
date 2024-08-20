67304935d2ea56df55038d92a14cf4b689809f8d
Ibrahimpalisse <ibrahim.vignes@laplateforme.io>
2024-08-20 15:36:05 +0200

    Mise à jour du CSS, modification de index.php, suppression de style.css

---
+++ includ/_Main.php
@@ -0,0 +1,84 @@
+<main>
+  <section class="container">
+    <div class="left">
+       <h1>
+          <strong>VIGNES IBRAHIM</strong>
+       </h1>
+       <hr class="bor1">
+       <h2>
+         Profil personnel
+        </h2>
+       <p>
+            14 av des Arnavaux<br>
+            13014 Marseille<br>
+            07.63.88.37.20<br>
+            Ibrahim.vignes@gmail.com<br>
+       </p>
+       <h3>
+         Compétences et savoir-faire
+      </h3>
+       <ul>
+           <li>
+             À l'écoute
+          </li>
+           <li>
+             Dynamique
+          </li>
+           <li>
+             Autonome
+          </li>
+           <li>
+             Sens de l'organisation
+          </li>
+           <li>
+              Préparation de colis
+            </li>
+           <li>
+              Sens de l'observatioz
+           </li>
+           <li>
+              Ponctuel
+           </li>
+           <li>
+             Souriant
+           </li>
+           <li>
+             Langue : Arabe littéraire
+           </li>
+       </ul>
+    </div>
+
+    <div class="right">
+        <div class="couleur_boite_right_1">
+            <h4 class="couleur_text_right">Expérience professionnelle</h4>
+            <p>- ENTREPRISE FAMILIALE - AU MAROC <br>  E-D-P ESSENCES DE PARFUMS</p>
+            <p>- Préparation de commande de parfum<br>
+               - Stage dans le numérique à l'école de la Plateforme (1 mois) 2024</p>
+        </div>
+
+        <div class="couleur_boite_right_1">
+            <h4 class="couleur_text_right">Éducation</h4>
+            <p>- École privée 2021-2023<br>
+               - École privée Lycée (Marrakech, Maroc)</p> 
+        </div>
+
+        <div class="couleur_boite_right_1">
+            <h5 class="couleur_text_right">Formations</h5> 
+           <p>2023-2024<br>
+               - CIERES Pic Axe 2 pour validation d'un projet professionnel - Marseille</p>
+            <p>2024<br>
+               - École de la Deuxième Chance<br>
+                 Remise à Niveau et construction du projet professionnel</p>
+        </div>
+
+        <div class="couleur_boite_right_1">
+           <h6 class="couleur_text_right">Intérêts et Loisirs</h6>
+           <ul>
+              <li> Les manhua (manga chinois)</li>
+              <li> Les manhwa (manga coréen)</li>
+              <li> Lire des nouvelles (xianxia)</li>
+           </ul>
+        </div>
+     </div>
+  </section>
+</main>
--- index.php
+++ index.php
@@ -1,87 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-             <link rel="stylesheet" href="style.css">
-    <title>Ibrahim vignes</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+ <?php
+ include('includ/_Head.php');
+ ?>
 <body>
-    <div>
-    <section class="v1">
-       <h1><strong>VIGNES IBRAHIM</strong> </h1>
-
-       <hr class="bor1"> </br>
-       <h2> Profil personnel</h2>
-       <P class="f1">
-        14 av des arnavaux</br>
-        13014 Marseille </br> 
-        07.63.88.37.20 <br>
-        Ibrahim.vignes@gmail.com </br>
-       
-       </P>
-        <h3>Competences et savoir-faire</h2>
-            <ul class="f1">
-                <li>A l ecoute</li>
-                <li> Dynamique</li>
-                <li>Autonome</li>
-                <li>Sens de l organisation</li>
-                <li> PrEparation de colis</li>
-                <li>Sens de l observation</li>
-                <li>Ponctuel</li>
-                <li>Souriant</li>
-                <li>  langue : Arabe littEraire</li>
-                
-            </ul>
-
-    </section>
-
-    
-    <section class="v2">
-        <div class="o1">
-            <h4 class="a1">Experience professionnelle</h4>
-                                                     </div>
-        <p>-ENTREPRISE FAMILIALE - AU MAROC
-            E-D-P  ESSENCES DE PARFUMS</p>
-
-       <p> - Preparation de commande de parfum
-           - un seage dans le numerique a l ecole de la
-             Plateforme (1 mois) 2024</p>
-                                    
-      <div class="o1">
-            <h4 class="a1">Education</h4> </div>
-          <p>Ecole privee   2021-2023</p>
-          <p>-Ecole privee Lycee (marrakech maroc)</p> 
-                                                    
-
-      <div class="o1">
-          <h5 class="a1">FORMATIONS</h5></div> 
-          <p> 2023-2024
-            -CIERES Pic Axe 2 pour validation a un projet   
-              professionnel - Marseille</p>
-           <p> -2024
-                Ecole de la Deuxième Chance 
-                Remise à Niveau et construction du projet   
-                professionnel     
-           </p>
-      <div class="o1">
-           <h6 class="a1">Interets et Loisirs</h6></div>
-           <ul>
-              <il>- Les manhua (manga chinois)<br/></il>
-              <il>- Les manhwa ( manga coreen)<br/></il>
-              <il>- Lire des nouvelle (xianxia)</il>
-           </ul>
-
-
-       
-    
-    </section>
-
-  </div>
-       
+     <?php
+     include('includ/_Main.php');
+     ?>
 </body>
 </html>
