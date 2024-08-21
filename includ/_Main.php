7048c5a55f937fbbb2280d5a89c1f1b90664884f
Ibrahimpalisse <ibrahim.vignes@laplateforme.io>
2024-08-21 11:50:02 +0200

    j'ai fini mon CV je veux metre mon CV en @media print

--- includ/_Main.php
+++ includ/_Main.php
@@ -4,25 +4,33 @@
        <h1>
           <strong>VIGNES IBRAHIM</strong>
        </h1>
+      
        <hr class="bor1">
        <h2>
-         Profil personnel
-        </h2>
-       <p>
-            14 av des Arnavaux<br>
-            13014 Marseille<br>
-            07.63.88.37.20<br>
-            Ibrahim.vignes@gmail.com<br>
-       </p>
+          Profil personnel
+       </h2>
+       <div class="colone">
+            <p>
+               14 av des Arnavaux</br>
+               13014 Marseille</br>
+               07.63.88.37.20</br>
+               Ibrahim.vignes@gmail.com
+            </p>
+            <p>Suivez-moi : 
+               <a class="suiver" href="https://github.com/Ibrahimpalisse?tab=repositories">GitHub</a>
+            </p>
+      </div>
+
+      
        <h3>
          Compétences et savoir-faire
       </h3>
        <ul>
            <li>
-             À l'écoute
+            🎧 À l'écoute
           </li>
            <li>
-             Dynamique
+            ⚡ Dynamique
           </li>
            <li>
              Autonome
@@ -50,33 +58,64 @@
 
     <div class="right">
         <div class="couleur_boite_right_1">
-            <h4 class="couleur_text_right">Expérience professionnelle</h4>
-            <p>- ENTREPRISE FAMILIALE - AU MAROC <br>  E-D-P ESSENCES DE PARFUMS</p>
-            <p>- Préparation de commande de parfum<br>
-               - Stage dans le numérique à l'école de la Plateforme (1 mois) 2024</p>
+            <h4 class="couleur_text_right">
+               Expérience professionnelle
+            </h4>
+            <p>
+               - Entreprise familiale - au maroc 
+                 e-d-p essences de parfums
+               - Préparation de commande de parfum
+            </p> 
+            <p>
+               - Stage dans le numérique à l'école de la Plateforme (1 mois) 2024
+            </p>
+            <p>
+               - Entreprise atelier 
+               -realisation des site web et mobil (HTML-CSS-PHP)
+            </p>
         </div>
 
         <div class="couleur_boite_right_1">
-            <h4 class="couleur_text_right">Éducation</h4>
-            <p>- École privée 2021-2023<br>
-               - École privée Lycée (Marrakech, Maroc)</p> 
+            <h4 
+               class="couleur_text_right">Éducation
+            </h4>
+
+            <p>
+               - École privée 2015-2023</br>
+               - École privée Lycée (Marrakech, Maroc)
+            </p> 
         </div>
 
         <div class="couleur_boite_right_1">
-            <h5 class="couleur_text_right">Formations</h5> 
-           <p>2023-2024<br>
+
+            <h5 class="couleur_text_right">
+               Formations
+            </h5> 
+           <p>2023-2024
                - CIERES Pic Axe 2 pour validation d'un projet professionnel - Marseille</p>
-            <p>2024<br>
-               - École de la Deuxième Chance<br>
-                 Remise à Niveau et construction du projet professionnel</p>
+            <p>2024
+               - École de la Deuxième Chance
+                 Remise à Niveau et construction du projet professionnel
+            </p>
+            <p>
+               -Développeur Web et Mobile a l'ecole La Plateforme  (2024)
+            </p>
         </div>
 
         <div class="couleur_boite_right_1">
-           <h6 class="couleur_text_right">Intérêts et Loisirs</h6>
+           <h6 class="couleur_text_right">
+              Intérêts et Loisirs
+          </h6>
            <ul>
-              <li> Les manhua (manga chinois)</li>
-              <li> Les manhwa (manga coréen)</li>
-              <li> Lire des nouvelles (xianxia)</li>
+               <li>
+                  Les manhua (manga chinois)
+               </li>
+               <li>
+                  Les manhwa (manga coréen)
+                  </li>
+               <li>
+                  Lire des nouvelles (xianxia)
+               </li>
            </ul>
         </div>
      </div>
