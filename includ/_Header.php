277a1fb9b2c4f2798ae70fb28965bc9a76263a32
Ibrahimpalisse <ibrahim.vignes@laplateforme.io>
2024-08-22 11:54:57 +0200

    j'ai ajouter du css et bouton sur le header pour routourner sur la page index.php de la page principale de plesk

--- includ/_Header.php
+++ includ/_Header.php
@@ -1,3 +1,6 @@
 <button class="imprimer" id="print-button" onclick="window.print()">
     Imprimer cette page
 </button>
+<button class="retour">
+ <a href="../../index.php">revenir a la page principale</a>   
+</button>
